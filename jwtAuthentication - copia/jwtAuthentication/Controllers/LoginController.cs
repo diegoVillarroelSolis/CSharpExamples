2e1df32801f02a8633e8eb5fda59be121ea7ceec
Diego Villarroel Solis <diego.villarroel@outlook.com>
2018-05-17 11:36:06 -0400

    request Token

---
+++ jwtAuthentication - copia/jwtAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jwtAuthentication.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Login")]
+    public class LoginController : Controller
+    {
+        // POST api/Login
+        [HttpPost]
+        public IActionResult ValidateUser([FromBody] UserDto user)
+        {
+            if (user.Name == "user" && user.Password == "secret")
+            {
+                return Ok("The user has been succesfully autthenticated");
+            }
+            return BadRequest("Could not verify username and password");
+        }
+
+        //[HttpPost]
+        //public IActionResult RequestToken([FromBody] UserDto user)
+        //{
+        //    if (user.Name == "user" && user.Password == "secret")
+        //    {
+        //        var claims = new[]
+        //        {
+        //            new Claim(ClaimTypes.Name, request.Username),
+        //            new Claim("CompletedBasicTraining", ""),
+        //            new Claim(CustomClaimTypes.EmploymentCommenced,
+        //                        new DateTime(2017,12,1).ToString(),
+        //                        ClaimValueTypes.DateTime)
+        //        };
+
+        //        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+        //        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        //        var token = new JwtSecurityToken(
+        //            issuer: "yourdomain.com",
+        //            audience: "yourdomain.com",
+        //            claims: claims,
+        //            expires: DateTime.Now.AddMinutes(30),
+        //            signingCredentials: creds);
+
+        //        return Ok(new
+        //        {
+        //            token = new JwtSecurityTokenHandler().WriteToken(token)
+        //        });
+        //    }
+
+        //    return BadRequest("Could not verify username and password");
+        //}
+
+        public class UserDto
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+
+        }
+    }
+}
--- jwtAuthentication/jwtAuthentication/Controllers/LoginController.cs
+++ jwtAuthentication/jwtAuthentication/Controllers/LoginController.cs
@@ -1,67 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 namespace jwtAuthentication.Controllers
 {
     [Produces("application/json")]
     [Route("api/Login")]
     public class LoginController : Controller
     {
-        // POST api/Login
-        [HttpPost]
-        public IActionResult ValidateUser([FromBody] UserDto user)
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
         {
-            if (user.Name == "user" && user.Password == "secret")
-            {
-                return Ok("The user has been succesfully autthenticated");
-            }
-            return BadRequest("Could not verify username and password");
+            _configuration = configuration;
         }
 
+        // POST api/Login
         //[HttpPost]
-        //public IActionResult RequestToken([FromBody] UserDto user)
+        //public IActionResult ValidateUser([FromBody] UserDto user)
         //{
         //    if (user.Name == "user" && user.Password == "secret")
         //    {
-        //        var claims = new[]
-        //        {
-        //            new Claim(ClaimTypes.Name, request.Username),
-        //            new Claim("CompletedBasicTraining", ""),
-        //            new Claim(CustomClaimTypes.EmploymentCommenced,
-        //                        new DateTime(2017,12,1).ToString(),
-        //                        ClaimValueTypes.DateTime)
-        //        };
-
-        //        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
-        //        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        //        var token = new JwtSecurityToken(
-        //            issuer: "yourdomain.com",
-        //            audience: "yourdomain.com",
-        //            claims: claims,
-        //            expires: DateTime.Now.AddMinutes(30),
-        //            signingCredentials: creds);
-
-        //        return Ok(new
-        //        {
-        //            token = new JwtSecurityTokenHandler().WriteToken(token)
-        //        });
+        //        return Ok("The user has been succesfully autthenticated");
         //    }
-
         //    return BadRequest("Could not verify username and password");
         //}
 
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] UserDto user)
+        {
+            if (user.Name == "user" && user.Password == "secret")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "mydomain.com",
+                    audience: "mydomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+
         public class UserDto
         {
-            [Required]
+            //[Required]
             public string Name { get; set; }
 
-            [Required]
+            //[Required]
             public string Password { get; set; }
 
         }
